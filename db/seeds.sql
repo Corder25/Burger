2efe0cd5efd104da482b13fef68b26da20c07616
Cam corder <corder2425@yahoo.com>
2021-01-14 10:03:20 -0600

    server and controllers

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burgers) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burgers) VALUES ('Jalapeno Cheese Burger');
+INSERT INTO burgers (burgers) VALUES ('Brisket Burger');
